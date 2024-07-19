67daa4dad88fd2b2e43948bb6ae774c940062955
SyedHassanMMU <1221301318@student.mmu.edu.my>
2024-07-19 23:16:03 +0800

    First commit

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ class App{
 		// Load a glTF resource
 		loader.load(
 			// resource URL
-			'Showroom.glb',
+			'college.glb',
 			// called when the resource is loaded
 			function ( gltf ) {
 
